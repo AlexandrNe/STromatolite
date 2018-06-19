72193e1a65c0b30068341a0102a3f9e9bd2abd0e
Alexandr Nedilko <admin.hcu@mail.ru>
2018-06-19 05:58:06 +0500

    CF

---
+++ Stromatolite/Models/Entities/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Stromatolite.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid ProductID { get; set; }
+
+        [DisplayName("Название")]
+        [StringLength(200)]
+        [Required]
+        public string Title { get; set; }
+
+        [DisplayName("Полное наименование")]
+        [StringLength(500)]
+        [Required]
+        public string TitleFull { get; set; }
+
+        [DisplayName("Артикул")]
+        [StringLength(25)]
+        public string Article { get; set; }
+
+        [DisplayName("Описание")]
+        [AllowHtml]
+        public string Description { get; set; }
+
+        [DisplayName("ЧПУ")]
+        public string SEOurl { get; set; }
+
+        [DisplayName("Опубликован")]
+        [DefaultValue(true)]
+        public bool Active { get; set; }
+
+        [DisplayName("Теги")]
+        [StringLength(500)]
+        public string Tags { get; set; }
+
+        [DisplayName("MetaDescription")]
+        [StringLength(300)]
+        public string MetaDescription { get; set; }
+
+        [DisplayName("MetaDescription")]
+        [StringLength(200)]
+        public string KeyWords { get; set; }
+
+        [DisplayName("Индекс сортировки")]
+        [Required]
+        [DefaultValue(500)]
+        public int Sort { get; set; }
+
+    }
+}
---
+++ Stromatolite/Models/StromatoliteModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Stromatolite.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class StromatoliteModel : DbContext
+    {
+        // Контекст настроен для использования строки подключения "StromatoliteModel" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Stromatolite.StromatoliteModel" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "StromatoliteModel" 
+        // в файле конфигурации приложения.
+        public StromatoliteModel()
+            : base("name=StromatoliteModel")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+         public virtual DbSet<Product> Products { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
